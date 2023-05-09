073798f3ff196e2e1d47bc67e1bba40f2455ca4e
Mochamad Armand Al Farizzi <armandfarizzi@gmail.com>
2023-05-09 14:59:15 +0700

    initial commit

---
+++ src/main/java/com/example/javaclass/JavaclassApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaclass/JavaclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
